1b736ca3ae13db192f52387e4bdc86625de881c5
Chenqi <1002652154@qq.com>
2022-01-26 09:38:55 +0800

    set geometry helper functions as constexpr

--- WndDesign/geometry/helper.h
+++ WndDesign/geometry/helper.h
@@ -7,29 +7,29 @@
 BEGIN_NAMESPACE(WndDesign)
 
 
-Size ExtendSize(Size size, int length) {
+constexpr Size ExtendSize(Size size, int length) {
 	int width = (int)size.width + length * 2, height = (int)size.height + length * 2;
 	return Size(width >= 0 ? width : 0, height >= 0 ? height : 0);
 }
 
-Rect ExtendRegion(Rect region, int length) {
+constexpr Rect ExtendRegion(Rect region, int length) {
 	return Rect(region.point - Vector(length, length), ExtendSize(region.size, length));
 }
 
-Size ExtendSize(Size size, Margin margin) {
+constexpr Size ExtendSize(Size size, Margin margin) {
 	int width = (int)size.width + margin.left + margin.right;
 	int height = (int)size.height + margin.top + margin.bottom;
 	return Size(width >= 0 ? width : 0, height >= 0 ? height : 0);
 }
 
-Rect ExtendRegion(Rect region, Margin margin) {
+constexpr Rect ExtendRegion(Rect region, Margin margin) {
 	return Rect(region.point - Vector(margin.left, margin.top), ExtendSize(region.size, margin));
 }
 
 
-uint square(int x) { return (uint)(x * x); };
+constexpr uint square(int x) { return (uint)(x * x); };
 
-bool PointInRoundedRectangle(Point point, Rect rect, uint radius) {
+constexpr bool PointInRoundedRectangle(Point point, Rect rect, uint radius) {
 	if (!rect.Contains(point)) { return false; } if (radius == 0) { return true; }
 	int x1 = rect.left() + (int)radius, x2 = rect.right() - (int)radius;
 	int	y1 = rect.top() + (int)radius, y2 = rect.bottom() - (int)radius;
