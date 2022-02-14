5196d0a236946b03edc3a073690d073f605fa680
Chenqi <1002652154@qq.com>
2022-02-14 17:17:28 +0800

    filter out minimized message at WM_SIZE

--- WndDesign/system/win32_api.cpp
+++ WndDesign/system/win32_api.cpp
@@ -108,7 +108,7 @@ LRESULT CALLBACK WndProc(HWND hwnd, UINT msg, WPARAM wparam, LPARAM lparam) {
 		case WM_WINDOWPOSCHANGED: return DefWindowProc(hwnd, msg, wparam, lparam);
 		case WM_MOVE: frame->SetPoint(Point((short)LOWORD(lparam), (short)HIWORD(lparam))); break;
 		case WM_SIZE: frame->SetStatus(static_cast<DesktopFrameApi::Status>(wparam <= 2 ? wparam : 2));
-			frame->SetSize(Size(LOWORD(lparam), HIWORD(lparam))); break;
+			if (wparam != SIZE_MINIMIZED) { frame->SetSize(Size(LOWORD(lparam), HIWORD(lparam))); } break;
 
 			// notifications
 		case WM_PAINT: {
