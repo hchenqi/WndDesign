8618c476a47c6fcc5aa50fd6e7e199667470752b
Chenqi <1002652154@qq.com>
2022-09-17 12:18:05 +0800

    minor fix for directx helper

--- WndDesign/system/directx_helper.h
+++ WndDesign/system/directx_helper.h
@@ -49,34 +49,17 @@ struct TextLayout : IDWriteTextLayout4 {};
 
 
 inline D2D1_POINT_2F AsD2DPoint(Point point) { return { point.x, point.y }; }
-
 inline D2D1_SIZE_F AsD2DSize(Size size) { return { size.width, size.height }; }
-
 inline D2D1_RECT_F AsD2DRect(Rect rect) { return { rect.left(), rect.top(), rect.right(), rect.bottom() }; }
-
 inline Point AsPoint(D2D1_POINT_2F point) { return Point(point.x, point.y); }
-
 inline Size AsSize(D2D1_SIZE_F size) { return Size(size.width, size.height); }
-
 inline Rect AsRect(D2D1_RECT_F rect) { return Rect(Point(rect.left, rect.top), Size(rect.right - rect.left, rect.bottom - rect.top)); }
 
+inline D2D1::Matrix3x2F AsD2DTransform(Transform transform) {return reinterpret_cast<const D2D1::Matrix3x2F&>(transform);}
+inline Transform AsTransform(D2D1::Matrix3x2F matrix) {return reinterpret_cast<const Transform&>(matrix);}
 
-inline D2D1::Matrix3x2F AsD2DTransform(Transform transform) {
-	return reinterpret_cast<const D2D1::Matrix3x2F&>(transform);
-}
-
-inline Transform AsTransform(D2D1::Matrix3x2F matrix) {
-	return reinterpret_cast<const Transform&>(matrix);
-}
-
-
-inline float OpacityAsFloat(uchar opacity) {
-	return opacity / (float)0xFF;
-}
-
-inline D2D1_COLOR_F AsD2DColor(Color color) {
-	return D2D1::ColorF(color.AsUnsigned(), OpacityAsFloat(color.alpha));
-}
+inline float OpacityAsFloat(uchar opacity) {return opacity / (float)0xFF;}
+inline D2D1_COLOR_F AsD2DColor(Color color) {return D2D1::ColorF(color.AsUnsigned(), OpacityAsFloat(color.alpha));}
 
 
 END_NAMESPACE(WndDesign)
