799d5a4c94674f9734ac2fd480c642204048a030
Chenqi <1002652154@qq.com>
2022-11-19 12:52:05 +0800

    Update FlowLayoutTest.h

--- Test/FlowLayoutTest.h
+++ Test/FlowLayoutTest.h
@@ -10,72 +10,65 @@
 #include "WndDesign/frame/PaddingFrame.h"
 #include "WndDesign/wrapper/Background.h"
 
-#include <cmath>
-#include <ctime>
-
 
 using namespace WndDesign;
 
 
 struct MainFrameStyle : TitleBarFrame::Style {
 	MainFrameStyle() {
-		width.normal(800px).max(100pct);
-		height.normal(500px).max(100pct);
+		width.normal(560px).max(100pct);
+		height.normal(180px).max(100pct);
 		position.setHorizontalCenter().setVerticalCenter();
-		border.width(4).color(Color::Violet);
+		border.width(4).radius(4).color(Color::CadetBlue);
 		title.assign(L"FlowLayoutTest");
 		title_bar.background(Color::CadetBlue);
-		title_format.font.size(18);
+		title_format.font.size(18).color(Color::White);
 	}
 };
 
 
 class MyFlowLayout : public Decorate<FlowLayout, SolidColorBackground> {
 public:
-	MyFlowLayout() : Base(25, 10, 5) { AppendChild(new AddButton); }
+	MyFlowLayout() : Base(25, 10, 5) {}
 private:
-	static auto Create(std::wstring text, Color border_color) {
-		struct EditBoxStyle : public EditBox::Style {
-			EditBoxStyle() {
-				paragraph.line_height(90pct);
-				font.family(L"Segoe UI").size(18);
-			}
-		};
-		return new BorderFrame{
-			Border(3px, 12px, border_color),
-			new PaddingFrame{
-				Padding(5, 0),
-				new ClipFrame<Auto, Assigned>{
-					new MaxFrame{
-						length_max,
-						new EditBox(EditBoxStyle(), text)
-					}
-				}
-			}
-		};
-	}
-	class AddButton : public Button<Auto, Assigned> {
-	public:
-		AddButton() : Button(60) {}
-	private:
-		MyFlowLayout& GetFlowLayout() const { return static_cast<MyFlowLayout&>(GetParent()); }
-	private:
-		std::wstring NextName() {
-			static const wchar* names[27] = { L"Alice", L"Bob", L"Carol", L"Dave", L"Eve", L"Francis", L"Grace",
-				L"Hans", L"Isabella", L"Jason", L"Kate", L"Louis", L"Margaret", L"Nathan", L"Olivia", L"Paul", L"Queen",
-				L"Richard", L"Susan", L"Thomas", L"Uma", L"Vivian", L"Winnie", L"Xander", L"Yasmine", L"Zach", L"You Win" };
-			static uint next = 0;
-			return names[next >= 26 ? next : next++];
-		}
-		Color GetRandomColor() {
-			static bool seeded = []() { srand((uint)time(nullptr)); return true; }();
-			Color color = 0; color.red = rand(); color.green = rand(); color.blue = rand(); return color;
-		}
-	private:
-		virtual void OnClick() override {
-			GetFlowLayout().AppendChild(Create(NextName(), GetRandomColor()));
+	struct EditBoxStyle : public EditBox::Style {
+		EditBoxStyle() {
+			paragraph.line_height(90pct);
+			font.family(L"Segoe UI").size(17);
 		}
 	};
+	std::wstring NextName() {
+		static const wchar* names[27] = {
+			L"Alice", L"Bob", L"Carol", L"Dave", L"Eve", L"Francis", L"Grace", L"Hans", L"Isabella",
+			L"Jason", L"Kate", L"Louis", L"Margaret", L"Nathan", L"Olivia", L"Paul", L"Queen", L"Richard",
+			L"Susan", L"Thomas", L"Uma", L"Vivian", L"Winnie", L"Xander", L"Yasmine", L"Zach", L"You Win"
+		};
+		static uint next = 0;
+		return names[next >= 26 ? next : next++];
+	}
+private:
+	virtual ref_ptr<WndObject> HitTest(Point& point) override {
+		ref_ptr<WndObject> wnd = Base::HitTest(point);
+		return wnd == nullptr ? this : wnd;
+	}
+	virtual void OnMouseMsg(MouseMsg msg) override {
+		if (msg.type == MouseMsg::LeftDown) {
+			AppendChild(
+				new BorderFrame{
+					Border(3px, 12px, Color::CadetBlue),
+					new PaddingFrame{
+						Padding(6, 0),
+						new ClipFrame<Auto, Assigned>{
+							new MaxFrame{
+								length_max,
+								new EditBox(EditBoxStyle(), NextName())
+							}
+						}
+					}
+				}
+			);
+		}
+	}
 };
 
 
@@ -84,7 +77,10 @@ int main() {
 		new TitleBarFrame{
 			MainFrameStyle(),
 			new ScrollBox{
-				new MyFlowLayout
+				new PaddingFrame{
+					Padding(10),
+					new MyFlowLayout
+				}
 			}
 		}
 	);
