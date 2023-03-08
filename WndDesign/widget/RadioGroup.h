87040216528cecabf5a2b09cfdd078c2fcb3a4ec
Chenqi <1002652154@qq.com>
2023-03-08 22:25:32 +0800

    add widget/RadioGroup.h

---
+++ WndDesign/widget/RadioGroup.h
@@ -0,0 +1,85 @@
+#pragma once
+
+#include "WndDesign/layout/ListLayout.h"
+#include "WndDesign/frame/PaddingFrame.h"
+#include "WndDesign/style/value_tag.h"
+#include "WndDesign/figure/shape.h"
+#include "WndDesign/common/function_traits.h"
+
+
+BEGIN_NAMESPACE(WndDesign)
+
+
+template<class T>
+struct RadioItem {
+	T value;
+	child_ptr<Assigned, Auto> child;
+	bool selected = false;
+};
+
+
+template<class T>
+class RadioGroup : public ListLayout<Vertical> {
+public:
+	template<class... Ts>
+	RadioGroup(std::function<void(const T&)> callback, Ts... child_args) :
+		ListLayout(0px, (new ItemFrame(*this, std::move(child_args)))...),
+		callback(callback),
+		selected_item(selected_item ? selected_item : nullptr) {
+	}
+
+private:
+	class ItemFrameBase : public PaddingFrame<Assigned, Auto> {
+	protected:
+		ItemFrameBase(RadioGroup& group, bool selected, child_ptr child) : PaddingFrame{
+			Padding(20px, 0px, 0px, 0px),
+			std::move(child)
+		}, selected(selected) {
+			if (selected) { group.selected_item = this; }
+		}
+	protected:
+		RadioGroup& GetParent() { return static_cast<RadioGroup&>(WndObject::GetParent()); }
+	private:
+		bool selected;
+	public:
+		void Select() { selected = true; Redraw(GetPaddingRegion()); }
+		void Unselect() { selected = false; Redraw(GetPaddingRegion()); }
+	private:
+		Rect GetPaddingRegion() const { return Rect(Point(0, 0), Size(padding.left, size.height)); }
+	private:
+		virtual void OnDraw(FigureQueue& figure_queue, Rect draw_region) override {
+			PaddingFrame::OnDraw(figure_queue, draw_region);
+			figure_queue.add(GetPaddingRegion().Center(), new Circle(6px, selected ? Color::Gray : color_transparent, 1.0f, Color::Gray));
+		}
+		virtual ref_ptr<WndObject> HitTest(Point& point) override { return selected ? PaddingFrame::HitTest(point) : this; }
+	};
+
+	template<class T>
+	class ItemFrame : public ItemFrameBase {
+	public:
+		ItemFrame(RadioGroup& group, RadioItem<T> item) : ItemFrameBase(group, item.selected, std::move(item.child)), value(std::move(item.value)) {}
+	private:
+		T value;
+	private:
+		void Select() { ItemFrameBase::GetParent().SelectItem(*this, value); }
+	private:
+		virtual void OnMouseMsg(MouseMsg msg) override { if (msg.type == MouseMsg::LeftDown) { Select(); } }
+	};
+
+private:
+	ref_ptr<ItemFrameBase> selected_item;
+	std::function<void(const T&)> callback;
+private:
+	void SelectItem(ItemFrameBase& item, const T& value) {
+		if (selected_item != nullptr) { selected_item->Unselect(); }
+		selected_item = &item; item.Select();
+		callback(value);
+	}
+};
+
+
+template<class Func, class... Ts>
+RadioGroup(Func, Ts...) -> RadioGroup<typename function_traits<Func>::template argument_type<0>>;
+
+
+END_NAMESPACE(WndDesign)
