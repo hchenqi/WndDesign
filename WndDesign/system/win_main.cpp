128c2b5fc0b011dae75dcaef8c196550b2ca269a
Chenqi <1002652154@qq.com>
2023-03-09 11:14:55 +0800

    remove main function return value

--- WndDesign/system/win_main.cpp
+++ WndDesign/system/win_main.cpp
@@ -2,11 +2,11 @@
 
 #include <Windows.h>
 
-extern int main();
+extern void main();
 
 int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine, int nShowCmd) {
 	SetThreadDpiAwarenessContext(DPI_AWARENESS_CONTEXT_PER_MONITOR_AWARE_V2);
-	int ret = main();
+	main();
 	WndDesign::global.Terminate();
-	return ret;
+	return 0;
 }
