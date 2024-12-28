dfcf45a391e151864712d0cbd83f7ea95602e28e
Chenqi <1002652154@qq.com>
2024-12-28 10:42:48 +0100

    Update TitleBarFrame.h

--- WndDesign/widget/TitleBarFrame.h
+++ WndDesign/widget/TitleBarFrame.h
@@ -1,130 +1,80 @@
 #pragma once
 
 #include "../frame/DesktopFrame.h"
+#include "../frame/WndFrameMutable.h"
+#include "../frame/PaddingFrame.h"
 #include "../frame/BorderFrame.h"
 #include "../frame/CenterFrame.h"
 #include "../frame/MaxFrame.h"
+#include "../frame/BackgroundFrame.h"
 #include "../layout/SplitLayout.h"
 #include "../layout/BarLayout.h"
 #include "../layout/ListLayout.h"
 #include "../control/TextBox.h"
 #include "../control/Button.h"
 #include "../wrapper/Background.h"
+#include "../wrapper/Cursor.h"
 #include "../style/length_style_helper.h"
 #include "../message/mouse_tracker.h"
+#include "../message/context.h"
 #include "../system/cursor.h"
 #include "../system/win32_aero_snap.h"
-#include "Tooltip.h"
+#include "../widget/Tooltip.h"
 
 
 BEGIN_NAMESPACE(WndDesign)
 
 
-class TitleBarFrame : public DesktopFrame {
+class TitleBarFrame : public DesktopFrame, ContextProvider {
 public:
-	using child_ptr = WndDesign::child_ptr<Assigned, Assigned>;
-	using child_ptr_menu = WndDesign::child_ptr<Auto, Assigned>;
+	using child_type = child_ptr<Assigned, Assigned>;
+	using child_type_menu = child_ptr<Auto, Assigned>;
 
 public:
-	struct TitleBarStyle {
-	public:
-		float _height = 30.0f;
-		float _max_title_length = 300.0f;
-		Color _background = Color::DarkGray;
-	public:
-		constexpr TitleBarStyle& height(float height) { _height = height; return *this; }
-		constexpr TitleBarStyle& max_title_length(float max_title_length) { _max_title_length = max_title_length; return *this; }
-		constexpr TitleBarStyle& background(Color background) { _background = background; return *this; }
+	struct TitleBarStyle : TextBlockStyle {
+		struct BarStyle {
+		public:
+			float _height = 30px;
+			float _max_title_length = 300px;
+			Color _background_color = Color::DarkGray;
+			Color _foreground_color = Color::White;
+		public:
+			constexpr BarStyle& height(float height) { _height = height; return *this; }
+			constexpr BarStyle& max_title_length(float max_title_length) { _max_title_length = max_title_length; return *this; }
+			constexpr BarStyle& background_color(Color background_color) { _background_color = background_color; return *this; }
+			constexpr BarStyle& foreground_color(Color foreground_color) { _foreground_color = foreground_color; return *this; }
+		}bar;
+
+		std::wstring text;
 	};
 
 	struct Style {
 		LengthStyle width;
 		LengthStyle height;
 		PositionStyle position;
+		Padding padding;
 		Border border;
-		TitleBarStyle title_bar;
-		std::wstring title;
-		TextBox::Style title_format;
 		Color background_color;
+		TitleBarStyle title;
 
 		Style() {
 			width.normal(800px).min(200px).max(100pct);
 			height.normal(500px).min(200px).max(100pct);
 			position.setHorizontalCenter().setVerticalCenter();
-			border.width(4).radius(4).color(Color::CadetBlue);
-			title_bar.background(Color::CadetBlue);
-			title.assign(L"TitleBarFrame");
-			title_format.font.size(16px).color(Color::White);
-			title_format.paragraph.line_spacing(100pct);
+			padding = Padding(2px);
+			border.width(2px).radius(2px).color(Color::CadetBlue);
 			background_color = Color::White;
+			title.bar.background_color(Color::CadetBlue);
+			title.text.assign(L"TitleBarFrame");
+			title.font.color(Color::White);
+			title.paragraph.line_spacing(100pct);
 		}
 	};
 
-public:
-	TitleBarFrame(Style style, child_ptr child, child_ptr_menu menu = new Placeholder<Auto, Assigned>(0.0f)) : DesktopFrame(
-		style.title,
-		new ResizeBorder(
-			style.border,
-			new SplitLayoutVertical(
-				new TitleBar(*this, style, std::move(menu)),
-				std::move(child)
-			)
-		)
-	), style(style), title(title) {}
-
-	// style
-private:
-	Style style;
-private:
-	ref_ptr<TextBox> title;
-public:
-	void SetTitle(std::wstring str) { title->Assign(str); DesktopFrame::SetTitle(str); }
-
-	// layout
-private:
-	virtual std::pair<Size, Size> CalculateMinMaxSize(Size size_ref) override {
-		return LengthStyleHelper::CalculateMinMaxSize(style.width, style.height, size_ref);
-	}
-	virtual Rect OnDesktopFrameSizeRefUpdate(Size size_ref) override {
-		Rect region = LengthStyleHelper::CalculateRegion(style.width, style.height, style.position, size_ref);
-		UpdateChildSizeRef(child, region.size);
-		return region;
-	}
-
-	// paint
-private:
-	virtual void OnDraw(FigureQueue& figure_queue, Rect draw_region) override {
-		Rect child_region = GetBorder().GetChildRegion();
-		figure_queue.add(child_region.point, new Rectangle(child_region.size, style.background_color));
-		DrawChild(child, point_zero, figure_queue, draw_region);
-	}
-
-	// state
-private:
-	bool IsMaximized() { return GetState() == State::Maximized; }
-	void MaximizeOrRestore() { if (GetState() == State::Normal) { Maximize(); } else if (GetState() == State::Maximized) { Restore(); } }
-private:
-	virtual void OnStateChange(State state) {
-		switch (state) {
-		case State::Normal: GetBorder().Restore(); break;
-		case State::Maximized: GetBorder().Hide(); break;
-		}
-	}
-
-	// child
-private:
+protected:
 	class ResizeBorder : public BorderFrame<Assigned, Assigned> {
 	public:
-		ResizeBorder(Border border, child_ptr child) : BorderFrame<Assigned, Assigned>(border, std::move(child)), border_copy(border) {
-			cursor = Cursor::Hide;
-		}
-	public:
-		BorderFrame::GetChildRegion;
-	protected:
-		Border border_copy;
-	public:
-		void Hide() { border = {}; }
-		void Restore() { border = border_copy; }
+		using BorderFrame::BorderFrame;
 	protected:
 		virtual void OnMouseMsg(MouseMsg msg) override {
 			if (msg.type == MouseMsg::Move || msg.type == MouseMsg::LeftDown) {
@@ -137,72 +87,30 @@ private:
 			}
 		}
 	};
-private:
-	ResizeBorder& GetBorder() { return static_cast<ResizeBorder&>(*child); }
 
-private:
-	class TitleBar : public Decorate<BarLayout, SolidColorBackground> {
+	class TitleBar : public Decorate<BarLayout, SolidColorBackground>, Context, ContextProvider {
 	public:
-		using Style = TitleBarFrame::Style;
-	public:
-		TitleBar(TitleBarFrame& frame, const Style& style, child_ptr_menu menu) : Base(
-			style.title_bar._height,
-			std::move(menu),
-			new ListLayout<Horizontal>(
-				0.0f,
-				new MinimizeButton(frame, style.title_bar._background, L"minimize"),
-				new MaximizeButton(frame, style.title_bar._background, L"maximize"),
-				new CloseButton(frame, style.title_bar._background, L"close")
-			),
-			new CenterFrame<Auto, Assigned>(
-				new MaxFrame(
-					Size(style.title_bar._max_title_length, length_max),
-					new Title(frame, *this, style)
-				)
-			)
-		), frame(frame) {
-			background = style.title_bar._background;
-		}
-	private:
-		TitleBarFrame& frame;
-	private:
-		MouseTracker mouse_tracker;
-	private:
-		virtual void OnMouseMsg(MouseMsg msg) override {
-			switch (mouse_tracker.Track(msg)) {
-			case MouseTrackMsg::LeftDown: AeroSnapDraggingEffect(*this); break;
-			case MouseTrackMsg::LeftDoubleClick: frame.MaximizeOrRestore(); break;
-			}
-		}
-
-	private:
-		class Title : public Decorate<TextBox, WndDesign::SolidColorBackground> {
+		class Title : public TextBox, Context {
 		public:
-			Title(TitleBarFrame& frame, TitleBar& title_bar, const TitleBarFrame::Style& style) :
-				Base(style.title_format, style.title), title_bar(title_bar) {
-				frame.title = this;
-				background = style.title_bar._background;
-			}
-		private:
-			TitleBar& title_bar;
-		private:
-			virtual void OnMouseMsg(MouseMsg msg) { title_bar.OnMouseMsg(msg); }
+			Title(const TitleBarStyle& style) : TextBox(style, style.text), Context(AsWndObject()) {}
+		protected:
+			virtual void OnMouseMsg(MouseMsg msg) { Context::Get<TitleBar>().OnMouseMsg(msg); }
 		};
 
-		class ButtonBase : public Button<Auto, Assigned> {
+	protected:
+		class ButtonBase : public Button<Auto, Assigned>, protected Context {
 		public:
-			ButtonBase(TitleBarFrame& frame, Color background, const std::wstring& tool_tip_text) : Button<Auto, Assigned>(50.0f), frame(frame), tool_tip_text(tool_tip_text) {
+			ButtonBase(Color background, Color foreground, const std::wstring& tooltip_text) : Button<Auto, Assigned>(50.0f), Context(AsWndObject()), foreground(foreground), tooltip_text(tooltip_text) {
 				this->background = this->background_normal = background;
 			}
 		protected:
-			TitleBarFrame& frame;
-		private:
-			std::wstring tool_tip_text;
-		private:
+			Color foreground;
+			std::wstring tooltip_text;
+		protected:
 			virtual void OnNotifyMsg(NotifyMsg msg) override {
 				Button::OnNotifyMsg(msg);
 				switch (msg) {
-				case NotifyMsg::MouseEnter: TooltipShow(tool_tip_text); break;
+				case NotifyMsg::MouseEnter: TooltipShow(tooltip_text); break;
 				case NotifyMsg::MouseLeave: TooltipHide(); break;
 				}
 			}
@@ -211,45 +119,138 @@ private:
 		class MinimizeButton : public ButtonBase {
 		public:
 			using ButtonBase::ButtonBase;
-		private:
+		protected:
 			virtual void OnDraw(FigureQueue& figure_queue, Rect draw_region) override {
 				ButtonBase::OnDraw(figure_queue, draw_region);
-				figure_queue.add(Point(20.0f, 15.0f), new Rectangle(Size(10.0f, 1.0f), 1.0f, Color::White));
+				figure_queue.add(Point(20.0f, 15.0f), new Rectangle(Size(10.0f, 1.0f), 1.0f, foreground));
 			}
-		private:
-			virtual void OnClick() override { frame.Minimize(); }
+		protected:
+			virtual void OnClick() override { Context::Get<TitleBarFrame>().Minimize(); }
 		};
 
 		class MaximizeButton : public ButtonBase {
 		public:
 			using ButtonBase::ButtonBase;
-		private:
+		protected:
 			virtual void OnDraw(FigureQueue& figure_queue, Rect draw_region) override {
 				ButtonBase::OnDraw(figure_queue, draw_region);
-				if (frame.IsMaximized()) {
-					figure_queue.add(Point(22.0f, 10.0f), new Rectangle(Size(8.0f, 8.0f), 1.0f, Color::White));
-					figure_queue.add(Point(20.0f, 12.0f), new Rectangle(Size(8.0f, 8.0f), background, 1.0f, Color::White));
+				if (Context::Get<TitleBarFrame>().IsMaximized()) {
+					figure_queue.add(Point(22.0f, 10.0f), new Rectangle(Size(8.0f, 8.0f), 1.0f, foreground));
+					figure_queue.add(Point(20.0f, 12.0f), new Rectangle(Size(8.0f, 8.0f), background, 1.0f, foreground));
 				} else {
-					figure_queue.add(Point(20.0f, 10.0f), new Rectangle(Size(10.0f, 10.0f), 1.0f, Color::White));
+					figure_queue.add(Point(20.0f, 10.0f), new Rectangle(Size(10.0f, 10.0f), 1.0f, foreground));
 				}
 			}
-		private:
-			virtual void OnClick() override { frame.MaximizeOrRestore(); }
+		protected:
+			virtual void OnClick() override { Context::Get<TitleBarFrame>().MaximizeOrRestore(); }
 		};
 
 		class CloseButton : public ButtonBase {
 		public:
 			using ButtonBase::ButtonBase;
-		private:
+		protected:
 			virtual void OnDraw(FigureQueue& figure_queue, Rect draw_region) override {
 				ButtonBase::OnDraw(figure_queue, draw_region);
-				figure_queue.add(Point(20.0f, 10.0f), new Line(Vector(10.0f, 10.0f), 1.0f, Color::White));
-				figure_queue.add(Point(20.0f, 20.0f), new Line(Vector(10.0f, -10.0f), 1.0f, Color::White));
+				figure_queue.add(Point(20.0f, 10.0f), new Line(Vector(10.0f, 10.0f), 1.0f, foreground));
+				figure_queue.add(Point(20.0f, 20.0f), new Line(Vector(10.0f, -10.0f), 1.0f, foreground));
 			}
-		private:
-			virtual void OnClick() override { frame.Destroy(); }
+		protected:
+			virtual void OnClick() override { Context::Get<TitleBarFrame>().Destroy(); }
 		};
+
+	public:
+		TitleBar(const TitleBarStyle::BarStyle& style, child_type_menu menu, alloc_ptr<Title> title) : Base(
+			style._height,
+			std::move(menu),
+			new ListLayout<Horizontal>(
+				0.0f,
+				new MinimizeButton(style._background_color, style._foreground_color, L"minimize"),
+				new MaximizeButton(style._background_color, style._foreground_color, L"maximize"),
+				new CloseButton(style._background_color, style._foreground_color, L"close")
+			),
+			new CenterFrame<Auto, Assigned>(
+				new MaxFrame(
+					Size(style._max_title_length, length_max),
+					std::move(title)
+				)
+			)
+		), Context(AsWndObject()), ContextProvider(AsWndObject()) {
+			background = style._background_color;
+		}
+
+	protected:
+		MouseTracker mouse_tracker;
+	protected:
+		virtual void OnMouseMsg(MouseMsg msg) override {
+			switch (mouse_tracker.Track(msg)) {
+			case MouseTrackMsg::LeftDown: AeroSnapDraggingEffect(*this); break;
+			case MouseTrackMsg::LeftDoubleClick: Context::Get<TitleBarFrame>().MaximizeOrRestore(); break;
+			}
+		}
 	};
+
+public:
+	TitleBarFrame(Style style, child_type child, child_type_menu menu = new Placeholder<Auto, Assigned>(0.0f)) : DesktopFrame(
+		style.title.text,
+		outer_frame = new WndFrameMutable(
+			new PaddingFrame(
+				style.padding,
+				border = new ResizeBorder(
+					style.border,
+					child_ptr<Assigned, Assigned>() = inner_frame = new WndFrameMutable(
+						new SplitLayoutVertical(
+							new TitleBar(
+								style.title.bar,
+								std::move(menu),
+								title = new TitleBar::Title(style.title)
+							),
+							new BackgroundFrame(
+								style.background_color,
+								std::move(child)
+							)
+						)
+					)
+				)
+			)
+		)
+	), ContextProvider(AsWndObject()), style(style) {}
+
+	// style
+protected:
+	Style style;
+protected:
+	ref_ptr<ResizeBorder> border;
+	ref_ptr<TitleBar::Title> title;
+public:
+	void SetTitle(std::wstring str) { title->Assign(str); DesktopFrame::SetTitle(str); }
+
+	// layout
+protected:
+	virtual std::pair<Size, Size> CalculateMinMaxSize(Size size_ref) override {
+		return LengthStyleHelper::CalculateMinMaxSize(style.width, style.height, size_ref);
+	}
+	virtual Rect OnDesktopFrameSizeRefUpdate(Size size_ref) override {
+		Rect region = LengthStyleHelper::CalculateRegion(style.width, style.height, style.position, size_ref);
+		UpdateChildSizeRef(child, region.size);
+		return region;
+	}
+
+	// state
+protected:
+	ref_ptr<WndFrameMutable> outer_frame;
+	ref_ptr<WndFrameMutable> inner_frame;
+	child_ptr<Assigned, Assigned> inner_frame_placeholder = new Placeholder<Assigned, Assigned>;
+	child_ptr<Assigned, Assigned> outer_frame_placeholder = child_ptr<Assigned, Assigned>();
+protected:
+	bool IsMaximized() { return GetState() == State::Maximized; }
+	void MaximizeOrRestore() { if (GetState() == State::Normal) { Maximize(); } else if (GetState() == State::Maximized) { Restore(); } }
+protected:
+	virtual void OnStateChange(State state) {
+		switch (state) {
+		case State::Normal: if (outer_frame_placeholder) { WndFrameMutable::Rotate(*inner_frame, *outer_frame, outer_frame_placeholder, inner_frame_placeholder); } break;
+		case State::Maximized: if (inner_frame_placeholder) { WndFrameMutable::Rotate(*inner_frame, inner_frame_placeholder, outer_frame_placeholder, *outer_frame); } break;
+		}
+	}
 };
 
 
